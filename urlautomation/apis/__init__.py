4fb8508cf0a219ff0790543a167b92767ebec20b
bgrisdale21 <2327815@chester.ac.uk>
2025-05-14 20:41:03 +0100

    Tidy up previous change

--- urlautomation/apis/__init__.py
+++ urlautomation/apis/__init__.py
@@ -1,6 +1,1 @@
-"""@package urlautomation.decoders
-This module contains implementations for retrieving of information
-from various APIs.
-"""
-
-import urlautomation.apis.crtsh as crtsh
+# This is a dummy file to make this directory a package.
